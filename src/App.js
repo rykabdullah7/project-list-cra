bf4dcfcb17054373c825a84a60d5c8c42525e1d2
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-08-19 16:48:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import LoginForm from './components/login';
 import ProjectList from './components/project-list';
 
+
+const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isLoggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,9 +26,18 @@ function App() {
   }
 
   return (
-    <>
-      {isLoggedIn ? <ProjectList /> : <LoginForm onLoginSuccess={handleLoginSuccess} />}
-    </>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginForm onLoginSuccess={handleLoginSuccess} />
+        </Route>
+        <PrivateRoute
+          path="/project-list"
+          component={ProjectList}
+          isLoggedIn={isLoggedIn}
+        />
+      </Switch>
+    </Router>
   );
 }
 
