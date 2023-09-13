1c1f7e79417864bc718315c9306039f6be63af06
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-09-13 18:52:47 +0500

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
 import styled from 'styled-components';
+import  Button  from 'react-bootstrap/Button';
 
 const StyledTableRow = styled.tr`
   display: ${({ hidden }) => (hidden ? 'none' : 'table-row')};
@@ -9,28 +10,37 @@ const StyledTableRow = styled.tr`
 
 function Project({ project, onStarClick, onHideClick }) {
 
-    
-  
-  
     return (
-        <StyledTableRow hidden={project.hidden}>
-            <td>
-                <span>{project.name}</span>
-                <button
-                    type="button"
-                    className={`star ${project.isStarred ? 'starred' : 'star-button'}`}
-                    onClick={onStarClick}
-                >
-                    <FaStar size={30} className="star-icon pb-2" />
-                </button>
-                <button
-                    className="btn btn-primary hid"
-                    onClick={onHideClick}
-                >
-                    Hide
-                </button>
-            </td>
-        </StyledTableRow>
+        
+            <StyledTableRow hidden={project.hidden}>
+                <td className='row p-5'>
+                    <div className='container'>
+                        <div className='id'>{project.projectId}</div>
+                        <div className='name'>{project.name}</div>
+                        <div className='budget'>{project.budget}</div>
+                        <div className='timeline'>{project.timeline}</div>
+                        <div className='description'>{project.description}</div>
+                        <div>
+                            <button
+                                className={`star ${project.isStarred ? 'starred' : 'star-button'}`}
+                                onClick={onStarClick}
+                            >
+                                <FaStar size={30} className="star-icon pb-2" />
+                            </button></div>
+                        <div>
+                            <Button
+                                className="hid"
+                                onClick={onHideClick}
+                            >
+                                Hide
+                            </Button>
+                        </div>
+                    </div>
+
+                </td>
+            </StyledTableRow>
+        
+
     )
 }
 
