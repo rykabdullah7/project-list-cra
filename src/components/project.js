8ffd3e69642d9fbd028bff2aea77c0b381a06459
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-09-18 14:02:08 +0500

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -4,6 +4,7 @@ import { FaStar } from 'react-icons/fa';
 import styled from 'styled-components';
 import  Button  from 'react-bootstrap/Button';
 
+
 const StyledTableRow = styled.tr`
   display: ${({ hidden }) => (hidden ? 'none' : 'table-row')};
 `;
@@ -13,21 +14,14 @@ function Project({ project, onStarClick, onHideClick }) {
     return (
         
             <StyledTableRow hidden={project.hidden}>
-                <td className='row p-5'>
+                <td className='row px-5  py-3'>
                     <div className='container'>
-                        <div className='id'>{project.projectId}</div>
+                        <div className='id px-4'>{project.projectId}</div>
                         <div className='name'>{project.name}</div>
                         <div className='budget'>{project.budget}</div>
                         <div className='timeline'>{project.timeline}</div>
                         <div className='description'>{project.description}</div>
-                        <div>
-                            <button
-                                className={`star ${project.isStarred ? 'starred' : 'star-button'}`}
-                                onClick={onStarClick}
-                            >
-                                <FaStar size={30} className="star-icon pb-2" />
-                            </button></div>
-                        <div>
+                        <div className='hide-btn px-3'>
                             <Button
                                 className="hid"
                                 onClick={onHideClick}
@@ -35,6 +29,14 @@ function Project({ project, onStarClick, onHideClick }) {
                                 Hide
                             </Button>
                         </div>
+                        <div className='star-btn px-3'>
+                            <button
+                                className={`star ${project.isStarred ? 'starred' : 'star-button'}`}
+                                onClick={onStarClick}
+                            >
+                                <FaStar size={30} className="star-icon pb-2" />
+                            </button></div>
+                        
                     </div>
 
                 </td>
