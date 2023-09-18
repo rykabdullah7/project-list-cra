f92d447e92933be06e2ea970159834a292dffa80
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-09-18 14:02:26 +0500

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -4,8 +4,20 @@ import Table from 'react-bootstrap/Table';
 
 function ProjectsTable({ children }) {
   return (
-    <Table  hover width="100%" className="custom-table">
+    <Table striped hover width="100%" className="custom-table">
+
       <tbody className="custom-tbody">
+        <td className='row head px-5 py-3'>
+          <div className='container'>
+            <div className='id px-4'>ID</div>
+            <div className='name'>Name</div>
+            <div className='budget'>Budget</div>
+            <div className='timeline'>Timeline</div>
+            <div className='description'>Description</div>
+            <div className=''></div>
+            <div></div>
+          </div>
+        </td>
         {children}
       </tbody>
     </Table>
